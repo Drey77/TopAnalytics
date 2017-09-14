cabd8e21652691253533b98496b7a0f50f0f2869
Drey77 <Zoldik77@hotmail.com>
2017-09-14 16:46:07 +0200

    modified filter check in pixelView from nickname to topuser_id

--- topanalytics/views.py
+++ topanalytics/views.py
@@ -20,21 +20,21 @@ class PixelView(generic.TemplateView):
 
 	@cache_control(must_revalidate=True, max_age=60)
 	def get(self, request, pixel):
-		nickname = request.GET.get('nickname')
 		topuser_id = request.GET.get('topuser_id')
+		nickname = request.GET.get('nickname')
 		profile_picture = request.GET.get('profile_picture')
 		gender = request.GET.get('gender')
-		print('The user\'s nickname is : {}, and his topuser_id is : {}, and his profile picture URL is : {}, and his gender is : {}'
-			.format(nickname, topuser_id, profile_picture, gender))
+		print('The user\'s topuser_id is : {}, and his nickname is : {}, and his profile picture URL is : {}, and his gender is : {}'
+			.format(topuser_id, nickname, profile_picture, gender))
 		#les {} servent à être remplacé par ce qui se trouve entre les parenthèses après .format, dans l'ordre.
-		entry = Meta_TopChretien.objects.filter(nickname=nickname)
+		entry = Meta_TopChretien.objects.filter(topuser_id=topuser_id)
 
 		if entry.exists():
 			print('The user is already registered in DB')
 		else:
 			new_user = Meta_TopChretien(nickname=nickname, topuser_id=topuser_id, profile_picture=profile_picture, gender=gender)
 			new_user.save()
-			print('Saving in database... checking nickname... here it is : ' + str(entry.first()))
+			print('Saving in database... checked topuser_id, here it is : ' + str(entry.first()))
 
 		#Rendering of the pixel
 		pixel_image = "\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00\x00\xff\xff\xff\x00\x00\x00\x21\xf9\x04\x01\x00\x00\x00\x00\x2c\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02\x44\x01\x00\x3b"
